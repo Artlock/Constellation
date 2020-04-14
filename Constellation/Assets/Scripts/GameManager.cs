091ea908f3b01057d42788ae17f440e5186efee2
Record <record.tristan@gmail.com>
2020-04-14 10:56:47 +0200

    Can create stars

---
+++ Constellation/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public LineRenderer line;
+    public Star starPrefab;
+    public Transform constellation;
+
+    public Color currentColor;
+
+    void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CreateTheStar();
+        }
+    }
+
+    // We Create The Stars - GUMIHO
+    public void CreateTheStar()
+    {
+        Star star = Instantiate(starPrefab, constellation);
+        Vector2 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        star.Initialize(currentColor, position);
+    }
+}
---
+++ Constellation/Assets/Scripts/Star.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    public SpriteRenderer display;
+
+    //INITIALIZE - She Her Her Hers
+    public void Initialize(Color color, Vector2 position)
+    {
+        display.color = color;
+        transform.position = position;
+    }
+}
