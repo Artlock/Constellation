cd848ce67ff215e304da9dc42ec3c9fe89ec0412
Record <record.tristan@gmail.com>
2020-04-14 11:16:33 +0200

    A line follow stars

---
+++ Constellation/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public static CameraManager instance;
+
+    public Vector2 followPosition;
+    public float speedFollow;
+    private Camera _camera;
+
+    void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+
+        _camera = GetComponentInChildren<Camera>();
+    }
+
+    void LateUpdate()
+    {
+        Vector3 position = transform.position;
+
+        position.x = Mathf.Lerp(position.x, followPosition.x, speedFollow * Time.deltaTime);
+        position.y = Mathf.Lerp(position.y, followPosition.y, speedFollow * Time.deltaTime);
+
+        transform.position = position;
+    }
+
+    //Follow Me - MUSE
+    public void FollowMe(Vector2 position)
+    {
+        followPosition = position;
+    }
+}
--- Constellation/Assets/Scripts/GameManager.cs
+++ Constellation/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,6 +12,8 @@ public class GameManager : MonoBehaviour
     public Transform constellation;
 
     public Color currentColor;
+    public Gradient gradient;
+    private List<Vector3> _positionStars = new List<Vector3>();
 
     void Awake()
     {
@@ -24,14 +27,25 @@ public class GameManager : MonoBehaviour
         {
             CreateTheStar();
         }
+        if (line.positionCount != 0)
+        {
+            Vector2 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            line.SetPosition(_positionStars.Count, position);
+        }
     }
 
     // We Create The Stars - GUMIHO
     public void CreateTheStar()
     {
+        line.colorGradient = gradient;
         Star star = Instantiate(starPrefab, constellation);
         Vector2 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        _positionStars.Add(position);
+
+        line.positionCount = _positionStars.Count +1;
+        line.SetPositions(_positionStars.ToArray());
 
         star.Initialize(currentColor, position);
+        CameraManager.instance.FollowMe(position);
     }
 }
