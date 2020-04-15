c6f4503e9073e45bea5f03fe35854e4a4d725fa7
Record <record.tristan@gmail.com>
2020-04-15 23:47:13 +0200

    Add sounds

---
+++ Constellation/Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+    public AudioSource sfx;
+
+    void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    //Sfx 8 Sound Effect - Noah Smith
+    public void SoundEffect(AudioClip clip)
+    {
+        sfx.pitch = Random.Range(1f, 2f);
+        sfx.PlayOneShot(clip);
+    }
+}
